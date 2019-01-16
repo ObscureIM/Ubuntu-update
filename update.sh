458b15abd3c5b973796335b015f5a4287a59cd3a
root <me@email.com>
2019-01-16 11:25:50 +0000

    Removed database by default

--- update.sh
+++ update.sh
@@ -5,6 +5,10 @@ cmake ..
 make
 cd src
 zip vSomeVersion.zip ./miner ./turtle-service ./Obsidiand
+rm -rf /root/.Obsidian
+cp ./Obsidiand /root/obsidiand-ha/
+cd /root/obsidiand-ha/
+forever start service.js
 cd /root/
 cd /root/sharedFolder/
 cp /root/obsidian/build/src/vSomeVersion.zip .
