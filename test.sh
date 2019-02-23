dacd5ec8ba664ab0bb36c0a702748646bf587c6f
root <you@example.com>
2019-02-23 06:10:59 +0000

    update scripts

---
+++ test.sh
@@ -0,0 +1,2 @@
+read version
+echo '{"tag_name":"'"$version"'","target_commitish": "master","name": "v'"$version"'","body": "Release of version "'$version'","draft": false,"prerelease": false}'
--- update.sh
+++ update.sh
@@ -1,16 +1,21 @@
-cd /root/obsidian/
+#stop all running processes
+forever stopall
+echo "Enter the new version to be published:"
+read version
+#update the current project
+cd /root/ObscureIM
 git pull
-cd /root/obsidian/build
+cd build
 cmake ..
 make
+#after making we zip the files
 cd src
-zip vSomeVersion.zip ./miner ./turtle-service ./Obsidiand
-rm -rf /root/.Obsidian
-cp ./Obsidiand /root/obsidiand-ha/
-cd /root/obsidiand-ha/
-forever start service.js
-cd /root/
-cd /root/sharedFolder/
-cp /root/obsidian/build/src/vSomeVersion.zip .
-python -m SimpleHTTPServer
-
+zip v$version-ubuntu.zip ./miner ./Obscured ./obscure-service ./zedwallet-beta ./wallet-api
+cp ./v$version-ubuntu.zip /root/
+unzip /root/v0.1.3-ubuntu.zip -d /root/
+cp /root/Obscured /root/obscured-ha/
+cp /root/obscure-service /root/walletd-ha/
+#for VPS 1
+forever start /root/obscured-ha/service.js
+forever start /root/walletd-ha/service.js
+forever start /root/Obscure-faucet-complete/init.js
